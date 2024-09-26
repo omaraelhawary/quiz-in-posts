986acaf9d5352ac2a28452cdd06169fcf812594e
Omar ElHawary <omaraelhawary@gmail.com>
2024-09-26 15:08:32 +0300

    Create Correct and Incorrect message

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '171197e77ff331dfc5fd');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '36c1b488bc7170633c0e');
--- build/frontend.js
+++ build/frontend.js
@@ -157,11 +157,47 @@ divsToUpdate.forEach(div => {
   div.classList.remove("posts-quiz-frontend-update");
 });
 function Quiz(props) {
+  const [isCorrect, setIsCorrect] = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)(undefined);
+  function handleAnswer(index) {
+    if (index == props.correctAnswer) {
+      setIsCorrect(true);
+    } else {
+      setIsCorrect(false);
+    }
+  }
   return (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)("div", {
     className: "posts-quiz-frontend"
   }, (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)("p", null, props.question), (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)("ul", null, props.answers.map(answer => {
-    return (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)("li", null, answer.name);
-  })));
+    return (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)("li", {
+      onClick: () => {
+        handleAnswer(answer.id);
+      }
+    }, answer.name);
+  })), (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)("div", {
+    className: "correct-message" + (isCorrect ? " correct-message--visible" : "")
+  }, (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    width: "24",
+    height: "24",
+    className: "bi bi-emoji-smile",
+    viewBox: "0 0 16 16"
+  }, (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)("path", {
+    d: "M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"
+  }), (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)("path", {
+    d: "M4.285 9.567a.5.5 0 0 1 .683.183A3.5 3.5 0 0 0 8 11.5a3.5 3.5 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5"
+  })), (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)("p", null, "That is correct")), (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)("div", {
+    className: "incorrect-message" + (!isCorrect ? " correct-message--visible" : "")
+  }, (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    width: "24",
+    height: "24",
+    className: "bi bi-emoji-frown",
+    viewBox: "0 0 16 16"
+  }, (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)("path", {
+    d: "M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"
+  }), (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)("path", {
+    d: "M4.285 12.433a.5.5 0 0 0 .683-.183A3.5 3.5 0 0 1 8 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.5 4.5 0 0 0 8 9.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5m4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5"
+  })), (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)("p", null, "Sorry, Try again")));
 }
 /******/ })()
 ;
